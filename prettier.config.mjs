0009f865c6fa216ead955546d2922ee2adc6209f
Sahil Basumatary <Basumatarysahil03@gmail.com>
2026-01-12 21:07:40 +0000

    configure prettier and husky

---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+const config = {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  printWidth: 80,
+  plugins: ['prettier-plugin-tailwindcss'],
+};
+export default config;
